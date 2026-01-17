4817d3f10261814453616aabca95c5041fbf1846
semanurdursun <semanurdursun1@stu.topkapi.edu.tr>
2026-01-17 09:07:01 +0300

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,22 @@
+function setTR() {
+    document.getElementById("navHome").innerHTML = "Ana Sayfa";
+    document.getElementById("navCV").innerHTML = "CV";
+    document.getElementById("navTimeline").innerHTML = "Zaman Çizelgesi";
+    document.getElementById("navContact").innerHTML = "İletişim";
+
+    if (document.getElementById("pageTitle")) {
+        document.getElementById("pageTitle").innerHTML = "Hoş Geldiniz";
+    }
+}
+
+function setEN() {
+    document.getElementById("navHome").innerHTML = "Home";
+    document.getElementById("navCV").innerHTML = "CV";
+    document.getElementById("navTimeline").innerHTML = "Timeline";
+    document.getElementById("navContact").innerHTML = "Contact";
+
+    if (document.getElementById("pageTitle")) {
+        document.getElementById("pageTitle").innerHTML = "Welcome";
+    }
+}
 
